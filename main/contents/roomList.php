699139968513b9c627469532501082b8a5f92543
Hajime Nishibe <kbc16a15@stu.kawahara.ac.jp>
2017-06-27 15:21:16 +0900

    [ver.0]以後developのblanchで開発してください

---
+++ main/contents/roomList.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>部屋一覧:待ち合わせするんです</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ main/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>トップページ:待ち合わせするんです</title>
+</head>
+<body>
+<a href="./contents/roomList.php">部屋一覧へ</a>
+</body>
+</html>
